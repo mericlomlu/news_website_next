fcf3e6385bdc31368c7885e625332ad4eea71bf8
Meriç Lomlu <mericlomlu@gmail.com>
2023-09-29 17:52:10 +0300

    Implemented Dark-Light mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,33 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import React from "react";
-import StyledComponentsRegistry from "@/lib/AntdRegistry";
+import { ThemeProvider } from "@/app/theme-provider";
+import { ThemeSwitcher } from "@/components/ThemeSwitcher";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'New Website Next',
-}
+  title: "New Website Next",
+  description: "Created by Meriç Lomlu",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
+  let switchThemeDuration = 1;
   return (
     <html lang="en">
-      <body className={inter.className}>
-      <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      <body
+        className={`${inter.className} bg-slate-50 dark:bg-[#0d1117] ${switchThemeDuration}`}
+      >
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <ThemeSwitcher />
+          <main>{children}</main>
+        </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
-'use client'
-import { ConfigProvider} from "antd";
-import theme from '../theme/themeConfig';
-import Navbar from "@/app/components/navbar";
+"use client";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-      <ConfigProvider theme={theme}>
-         <main className="w-full min-h-screen">
-             <Navbar/>
-         </main>
-      </ConfigProvider>
-  )
+    <main className="min-h-screen w-full">
+      <Navbar />
+    </main>
+  );
 }
