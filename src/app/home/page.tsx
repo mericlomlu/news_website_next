028ccbc512376f3c1568c9ae26c80bea2922facd
Meriç Lomlu <mericlomlu@gmail.com>
2023-10-24 03:03:42 +0300

    News Card

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import { URLS } from "../../../public/constants/api-urls";
 import { getTopHeadlines } from "@/redux/actions/topHeadlinesActions";
 import React, { useEffect } from "react";
+import { NewsCard } from "@/components/NewsCard";
 
 export default function Page() {
   const topHeadlinesData = useSelector(
@@ -25,6 +26,17 @@ export default function Page() {
   return (
     <div className="flex min-h-[55rem] w-full flex-col items-center justify-center gap-4">
       <p>Home Page</p>
+      {topHeadlinesData?.articles?.map((element: any) => {
+        <NewsCard
+          title={element?.title}
+          description={element?.description}
+          url={element?.url}
+          imgUrl={element?.urlToImage}
+          publishedAt={element?.publishedAt}
+          sourceName={element?.source?.name}
+          author={element?.author}
+        />;
+      })}
     </div>
   );
 }
---
+++ src/components/NewsCard.tsx
@@ -0,0 +1,71 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { Moon, Sun } from "../../public/constants/icons";
+import Image from "next/image";
+
+const something_went_wrong = require("../../public/assets/something_went_wrong.svg");
+
+const testDescription =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel egestas dolor, nec dignissim metus. Donec augue elit, rhoncus ac sodales id, porttitor vitae est. Donec laoreet rutrum libero sed pharetra." +
+  "Donec vel egestas dolor, nec dignissim metus. Donec augue elit, rhoncus ac sodales id, porttitor vitae est. Donec laoreet rutrum libero sed pharetra. Duis a arcu convallis, gravida purus eget, mollis diam.";
+
+type NewsCardProps = {
+  title?: string;
+  description?: string;
+  url?: string;
+  imgUrl?: string;
+  publishedAt?: string;
+  sourceName?: string;
+  author?: string;
+};
+
+export const NewsCard = (props: NewsCardProps) => {
+  const [isClicked, setIsClicked] = useState<boolean>(false);
+  const [buttonLabel, setButtonLabel] = useState<string>("Git");
+
+  const onClick = () => {
+    setIsClicked(true);
+    setButtonLabel("Okundu");
+  };
+
+  return (
+    <div className="flex h-96 w-72 flex-col items-center gap-2 rounded-xl border-4 border-neonCarrot-500">
+      <Image
+        src={props?.imgUrl || something_went_wrong}
+        width={284}
+        height={158}
+        alt="Something_Went_Wrong"
+        style={{
+          objectFit: "contain",
+          borderTopLeftRadius: 8,
+          borderTopRightRadius: 8,
+        }}
+      />
+      <div className="flex flex-row justify-around self-stretch px-4">
+        <p className="text-left text-sm font-thin text-zinc-900">
+          {props?.sourceName || "Google"}
+        </p>
+        <p className="text-right text-sm font-thin text-zinc-900">
+          {props?.publishedAt || "11/04/1996"}
+        </p>
+      </div>
+      <p className="text-m px-4 text-center font-bold text-zinc-950">
+        {props?.title || "Test Başlığı"}
+      </p>
+      <p className="line-clamp-3 px-4 text-left text-sm font-thin text-zinc-900">
+        {props?.description || testDescription}
+      </p>
+      <div className="flex flex-row items-center justify-around w-full justify-self-end pt-4">
+        <p className="text-left text-sm font-thin text-zinc-900">
+          {props?.author || "Meriç Lomlu"}
+        </p>
+        <button
+          className="neonCarrot-default-gradient flex items-center justify-center self-center rounded-md border-2 border-zinc-50 p-2 dark:border-neonCarrot-950 max-[1280px]:h-10  max-[1280px]:w-20"
+          onClick={() => onClick()}
+        >
+          <a href={props?.url}>{buttonLabel}</a>
+        </button>
+      </div>
+    </div>
+  );
+};
