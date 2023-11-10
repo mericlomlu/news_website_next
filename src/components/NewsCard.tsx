766e5fcd86ede9007a6661aac3fbfcd9478fc572
Meriç Lomlu <mericlomlu@gmail.com>
2023-11-10 05:02:24 +0300

    Configs

--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -1,6 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import { Moon, Sun } from "../../public/constants/icons";
+import React, { useState } from "react";
 import Image from "next/image";
 
 const something_went_wrong = require("../../public/assets/something_went_wrong.svg");
@@ -29,7 +28,7 @@ export const NewsCard = (props: NewsCardProps) => {
   };
 
   return (
-    <div className="flex-initial h-96 w-72 flex-col items-center gap-4 rounded-xl border-4 border-neonCarrot-500 basis-1/2 md:basis-1/3 lg:basis-1/4 xl:basis-1/6">
+    <div className="h-96 w-72 flex-initial basis-1/2 flex-col items-center gap-4 rounded-xl border-4 border-neonCarrot-500 md:basis-1/3 lg:basis-1/4 xl:basis-1/6">
       <Image
         src={props?.imgUrl || something_went_wrong}
         width={284}
@@ -39,7 +38,7 @@ export const NewsCard = (props: NewsCardProps) => {
           objectFit: "contain",
           borderTopLeftRadius: 8,
           borderTopRightRadius: 8,
-          width: '100%'
+          width: "100%",
         }}
       />
       <div className="flex flex-row justify-around self-stretch px-4">
@@ -50,18 +49,18 @@ export const NewsCard = (props: NewsCardProps) => {
           {props?.publishedAt || "11/04/1996"}
         </p>
       </div>
-      <p className="text-m px-4 text-center font-bold text-zinc-950 line-clamp-1">
+      <p className="text-m line-clamp-1 px-4 text-center font-bold text-zinc-950">
         {props?.title || "Test Başlığı"}
       </p>
       <p className="line-clamp-3 px-4 text-center text-sm font-thin text-zinc-900">
         {props?.description || testDescription}
       </p>
-      <div className="flex flex-row items-center justify-around w-full justify-self-end pt-4">
+      <div className="flex w-full flex-row items-center justify-around justify-self-end pt-4">
         <p className="text-left text-sm font-thin text-zinc-900">
           {props?.author || "Meriç Lomlu"}
         </p>
         <button
-          className="neonCarrot-default-gradient flex items-center justify-center self-center rounded-md border-2 border-zinc-50 p-2 dark:border-neonCarrot-950 max-[1280px]:h-10  max-[1280px]:w-20 justify-self-end"
+          className="neonCarrot-default-gradient flex items-center justify-center self-center justify-self-end rounded-md border-2 border-zinc-50 p-2 dark:border-neonCarrot-950  max-[1280px]:h-10 max-[1280px]:w-20"
           onClick={() => onClick()}
         >
           <a href={props?.url}>{buttonLabel}</a>
