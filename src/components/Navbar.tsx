e8dd928b6e513da71e464379f61a7fead7a2b684
Meriç Lomlu <mericlomlu@gmail.com>
2024-03-05 19:12:27 +0300

    Started navbar fixing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -42,16 +42,15 @@ const Navbar = () => {
   }, [language]);
 
   return (
-    <nav className="fixed top-0 flex h-auto min-h-[3rem] w-full items-end justify-between overflow-hidden bg-regentBlue-400 px-4 dark:bg-regentBlue-700">
+    <nav className="fixed top-0 flex h-auto max-h-[3rem] min-h-[3rem] w-full items-end gap-4 overflow-hidden bg-regentBlue-400 px-4 dark:bg-regentBlue-700">
       <div className="hidden items-center justify-center self-center min-[1280px]:flex">
         <Image src={Newspaper} alt="Newspaper" width={40} height={40} />
       </div>
-      <div className="flex items-center justify-center self-center min-[1280px]:hidden">
-        <Image src={Newspaper} alt="Newspaper" width={32} height={32} />
-      </div>
       <Link
         href={"/home"}
-        className={pathName === "/home" ? "nav-link-active" : "nav-link"}
+        className={
+          pathName === "/home" ? "nav-link-active flex-1" : "nav-link flex-1"
+        }
       >
         <Home width={20} />
         <p className="line-clamp-1 hidden xl:flex">
@@ -60,7 +59,11 @@ const Navbar = () => {
       </Link>
       <Link
         href={"/business"}
-        className={pathName === "/business" ? "nav-link-active" : "nav-link"}
+        className={
+          pathName === "/business"
+            ? "nav-link-active flex-1"
+            : "nav-link flex-1"
+        }
       >
         <BriefCase width={20} />
         <p className="line-clamp-1 hidden xl:flex"> {navLabels?.BUSINESS}</p>
@@ -68,51 +71,64 @@ const Navbar = () => {
       <Link
         href={"/entertainment"}
         className={
-          pathName === "/entertainment" ? "nav-link-active" : "nav-link"
+          pathName === "/entertainment"
+            ? "nav-link-active flex-1"
+            : "nav-link flex-1"
         }
       >
         <HeadPhones width={20} />
-        <p className="line-clamp-1 hidden xl:flex">
-          {" "}
-          {navLabels?.ENTERTAINMENT}
-        </p>
+        <div className=" hidden xl:flex">
+          <p className="line-clamp-1">{navLabels?.ENTERTAINMENT}</p>
+        </div>
       </Link>
       <Link
         href={"/general"}
-        className={pathName === "/general" ? "nav-link-active" : "nav-link"}
+        className={
+          pathName === "/general" ? "nav-link-active flex-1" : "nav-link flex-1"
+        }
       >
         <Tv width={20} />
         <p className="line-clamp-1 hidden xl:flex"> {navLabels?.GENERAL}</p>
       </Link>
       <Link
         href={"/health"}
-        className={pathName === "/health" ? "nav-link-active" : "nav-link"}
+        className={
+          pathName === "/health" ? "nav-link-active flex-1" : "nav-link flex-1"
+        }
       >
         <Activity width={20} />
         <p className="line-clamp-1 hidden xl:flex"> {navLabels?.HEALTH}</p>
       </Link>
       <Link
         href={"/science"}
-        className={pathName === "/science" ? "nav-link-active" : "nav-link"}
+        className={
+          pathName === "/science" ? "nav-link-active flex-1" : "nav-link flex-1"
+        }
       >
         <Thermometer width={20} />
         <p className="line-clamp-1 hidden xl:flex"> {navLabels?.SCIENCE}</p>
       </Link>
       <Link
         href={"/sports"}
-        className={pathName === "/sports" ? "nav-link-active" : "nav-link"}
+        className={
+          pathName === "/sports" ? "nav-link-active flex-1" : "nav-link flex-1"
+        }
       >
         <Dribbble width={20} />
         <p className="line-clamp-1 hidden xl:flex"> {navLabels?.SPORTS}</p>
       </Link>
       <Link
         href={"/technology"}
-        className={pathName === "/technology" ? "nav-link-active" : "nav-link"}
+        className={
+          pathName === "/technology"
+            ? "nav-link-active flex-1"
+            : "nav-link flex-1"
+        }
       >
         <SmartPhone width={20} />
         <p className="hidden xl:flex "> {navLabels?.TECHNOLOGY}</p>
       </Link>
-      <div className="flex items-center justify-center gap-2 self-center">
+      <div className=" flex items-center justify-center gap-2 self-center">
         <LanguageSwitcher />
         <ThemeSwitcher />
       </div>
